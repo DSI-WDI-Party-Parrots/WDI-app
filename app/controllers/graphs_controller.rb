97ee47bc00a73d242ea358d5bd94fa314dd5f347
James Johnson <jameswj111@gmail.com>
2016-08-05 09:41:28 -0400

    add javascript

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,4 +1,8 @@
 class GraphsController < ApplicationController
   def index
+    # url = "http://localhost:5000/predict"
+    # response = HTTParty.get(url)
+    # parsed_body = JSON.parse(response.body)
+    # render json: parsed_body
   end
 end
